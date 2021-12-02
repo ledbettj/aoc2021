738908d13120b28e3e01914ed499955e13dcd661
John Ledbetter <john.ledbetter@grayscaleapp.com>
2021-12-02 13:02:13 -0500

    use TryFrom

--- src/day2.rs
+++ src/day2.rs
@@ -9,39 +9,51 @@ enum Command {
 type Position = (isize, isize);
 type Position3 = (isize, isize, isize);
 
-impl From<&str> for Command {
-  fn from(value: &str) -> Self {
+#[derive(Debug, Clone)]
+struct CommandParseError(&'static str);
+
+impl TryFrom<&str> for Command {
+  type Error = CommandParseError;
+
+  fn try_from(value: &str) -> Result<Self, Self::Error> {
     let mut parts = value.split_ascii_whitespace();
-    let cmd = parts.next().unwrap();
-    let amount: isize = parts.next().unwrap().parse().unwrap();
+    let cmd = parts.next().ok_or(CommandParseError("No direction"))?;
+
+    let amount: isize = parts
+      .next()
+      .ok_or(CommandParseError("No amount"))?
+      .parse()
+      .map_err(|_| CommandParseError("Invalid amount"))?;
 
     match cmd {
-      "forward" => Command::Forward(amount),
-      "down"    => Command::Down(amount),
-      "up"      => Command::Up(amount),
-      _         => panic!("unexpected command"),
+      "forward" => Ok(Command::Forward(amount)),
+      "down" => Ok(Command::Down(amount)),
+      "up" => Ok(Command::Up(amount)),
+      _ => Err(CommandParseError("Unknown direction")),
     }
   }
 }
 
 impl Command {
-  fn load_list<'a, I: Iterator<Item = &'a str>>(lines: I) -> Vec<Command> {
-    lines.map(|line| line.into()).collect()
+  fn load_list<'a, I: Iterator<Item = &'a str>>(
+    lines: I,
+  ) -> Result<Vec<Command>, CommandParseError> {
+    lines.map(|line| line.try_into()).collect()
   }
 
   fn exec(&self, pos: Position) -> Position {
     match self {
       Command::Forward(n) => (pos.0 + n, pos.1),
-      Command::Down(n)    => (pos.0, pos.1 + n),
-      Command::Up(n)      => (pos.0, pos.1 - n),
+      Command::Down(n) => (pos.0, pos.1 + n),
+      Command::Up(n) => (pos.0, pos.1 - n),
     }
   }
 
   fn exec_aim(&self, pos: Position3) -> Position3 {
     match self {
       Command::Forward(n) => (pos.0 + n, pos.1 + pos.2 * n, pos.2),
-      Command::Down(n)    => (pos.0, pos.1, pos.2 + n),
-      Command::Up(n)      => (pos.0, pos.1, pos.2 - n),
+      Command::Down(n) => (pos.0, pos.1, pos.2 + n),
+      Command::Up(n) => (pos.0, pos.1, pos.2 - n),
     }
   }
 
@@ -60,16 +72,15 @@ mod tests {
 
   #[test]
   fn part1_solution() {
-    let list = Command::load_list(INPUT.lines());
+    let list = Command::load_list(INPUT.lines()).expect("Failed to load commands");
     let initial = (0, 0);
     let result = Command::exec_list(initial, &list);
     assert_eq!(result.0 * result.1, 2150351);
   }
 
-
   #[test]
   fn part2_solution() {
-    let list = Command::load_list(INPUT.lines());
+    let list = Command::load_list(INPUT.lines()).expect("Failed to load commands");
     let initial = (0, 0, 0);
     let result = Command::exec_list_aim(initial, &list);
     assert_eq!(result.0 * result.1, 1842742223);
