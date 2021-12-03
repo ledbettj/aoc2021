9100b613f0cabead105ea4ade4c4a420767e1aba
John Ledbetter <john.ledbetter@gmail.com>
2021-12-03 09:41:49 -0500

    d3p2 sucked

--- src/day3.rs
+++ src/day3.rs
@@ -19,7 +19,7 @@ fn input() -> Vec<&'static str> {
   INPUT.lines().collect()
 }
 
-fn calc_rates(input: &[&str]) -> (usize, usize) {
+fn bit_tallies(input: &[&str]) -> Vec<[usize; 2]> {
   let mut tallies : Vec<[usize; 2]> = vec![];
   for i in 0..input[0].len() {
     tallies.push([0, 0]);
@@ -32,6 +32,11 @@ fn calc_rates(input: &[&str]) -> (usize, usize) {
     }
   }
 
+  tallies
+}
+
+fn calc_gamma_epsilon(input: &[&str]) -> (usize, usize) {
+  let mut tallies = bit_tallies(&input);
   let mut gamma = 0;
   let mut epsilon = 0;
 
@@ -48,28 +53,69 @@ fn calc_rates(input: &[&str]) -> (usize, usize) {
   (gamma, epsilon)
 }
 
+fn calc_oxygen(input: &[&str]) -> usize {
+  let mut values : Vec<Vec<char>> = input.iter().map(|s| s.chars().collect()).collect();
+
+  for pos in 0..input[0].len() {
+    let ones = values.iter().filter(|s| s[pos] == '1').count();
+    let zeros = values.len() - ones;
+    let ch = if ones >= zeros { '1' } else { '0' };
+    values = values.iter().filter(|s| s[pos] == ch).cloned().collect();
+
+    if values.len() == 1 {
+      break;
+    }
+  }
+
+  usize::from_str_radix(&values[0].iter().collect::<String>(), 2).unwrap()
+}
+
+
+fn calc_co2(input: &[&str]) -> usize {
+  let mut values : Vec<Vec<char>> = input.iter().map(|s| s.chars().collect()).collect();
+
+  for pos in 0..input[0].len() {
+    let ones = values.iter().filter(|s| s[pos] == '1').count();
+    let zeros = values.len() - ones;
+    let ch = if ones >= zeros { '0' } else { '1' };
+    values = values.iter().filter(|s| s[pos] == ch).cloned().collect();
+
+    if values.len() == 1 {
+      break;
+    }
+  }
+
+  usize::from_str_radix(&values[0].iter().collect::<String>(), 2).unwrap()
+}
+
+
 #[cfg(test)]
 mod tests {
   use super::*;
 
   #[test]
   fn part1_example() {
-    assert_eq!(calc_rates(&SAMPLE), (22, 9));
+    assert_eq!(calc_gamma_epsilon(&SAMPLE), (22, 9));
   }
 
   #[test]
   fn part1_solution() {
-    let (g, e) = calc_rates(&input());
+    let (g, e) = calc_gamma_epsilon(&input());
     assert_eq!(g * e, 1092896);
   }
 
   #[test]
   fn part2_example() {
+    let o = calc_oxygen(&SAMPLE);
+    let co2 = calc_co2(&SAMPLE);
 
+    assert_eq!((o, co2), (23, 10));
   }
 
   #[test]
   fn part2_solution() {
-
+    let o = calc_oxygen(&input());
+    let co2 = calc_co2(&input());
+    assert_eq!(o * co2, 4672151);
   }
 }
