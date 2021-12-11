eb74b60fce28fe89197fee0b6a3e4e0622076ca5
John Ledbetter <john.ledbetter@grayscaleapp.com>
2021-12-11 12:02:01 -0500

    d11p2

--- src/day11.rs
+++ src/day11.rs
@@ -36,17 +36,17 @@ impl FromStr for Grid {
 
 impl Grid {
   fn step(&mut self) -> usize {
+    // set of points that flashed this iteration
     let mut seen : HashSet<Point> = HashSet::new();
+
     // first increment all cells
     self
       .cells
       .iter_mut()
-      .for_each(|(_, value)|{
-        *value += 1;
-      });
+      .for_each(|(_, value)| *value += 1 );
 
-    // process flash
     loop {
+      // find cells that should flash
       let flashes : Vec<Point> = self
         .cells
         .iter()
@@ -55,19 +55,23 @@ impl Grid {
         .cloned()
         .collect::<Vec<Point>>();
 
+      // mark as seen so we don't double flash
       for f in &flashes {
         seen.insert(*f);
       }
 
+      // find neighbors of flashes
       let to_increment = flashes
         .iter()
         .flat_map(|&point| self.neighbors(point))
         .collect::<Vec<Point>>();
 
+      // no neighbors to increment means we're done
       if to_increment.is_empty() {
         break;
       }
 
+      // increment neighbors
       for point in to_increment {
         self.cells
           .get_mut(&point)
@@ -75,18 +79,21 @@ impl Grid {
       }
     }
 
-    // set flashes to zero
+    // set all cells which flashed to zero
     for pos in &seen {
       self.cells.get_mut(&pos).map(|v| *v = 0);
     }
 
+    // total number of flashes this iteration
     seen.len()
   }
 
   fn run(&mut self, steps: usize) -> usize {
-    (0..steps)
-      .map(|_| self.step())
-      .sum()
+    (0..steps).map(|_| self.step()).sum()
+  }
+
+  fn run_until_sync(&mut self) -> Option<usize> {
+    (1..usize::MAX).find(|_| self.step() == self.cells.len())
   }
 
   fn neighbors<'a>(&'a self, pos: Point) -> impl Iterator<Item=Point> + 'a {
@@ -115,11 +122,13 @@ mod tests {
 
   #[test]
   fn part2_example() {
-
+    let mut g : Grid = SAMPLE.parse().expect("shit");
+    assert_eq!(g.run_until_sync(), Some(195));
   }
 
   #[test]
   fn part2_solution() {
-
+    let mut g : Grid = INPUT.parse().expect("shit");
+    assert_eq!(g.run_until_sync(), Some(251));
   }
 }
